45ced1156db22ddf23dea333448e4c8dd27b365b
boB Rudis <bob@rud.is>
2018-07-26 20:17:49 -0400

    tweaks

--- src/main/java/hrbrmstr/drill/udf/SuffixExtract.java
+++ src/main/java/hrbrmstr/drill/udf/SuffixExtract.java
@@ -5,7 +5,7 @@ import org.apache.drill.exec.expr.DrillSimpleFunc;
 import org.apache.drill.exec.expr.annotations.FunctionTemplate;
 import org.apache.drill.exec.expr.annotations.Output;
 import org.apache.drill.exec.expr.annotations.Param;
-import org.apache.drill.exec.expr.holders.NullableVarCharHolder;
+import org.apache.drill.exec.expr.holders.VarCharHolder;
 import org.apache.drill.exec.vector.complex.writer.BaseWriter;
 
 import crawlercommons.domains.EffectiveTldFinder;
@@ -20,7 +20,7 @@ import javax.inject.Inject;
 )
 public class SuffixExtract implements DrillSimpleFunc {
   
-  @Param NullableVarCharHolder dom_string;
+  @Param VarCharHolder dom_string;
   
   @Output BaseWriter.ComplexWriter out;
   
@@ -36,93 +36,92 @@ public class SuffixExtract implements DrillSimpleFunc {
       dom_string.start, dom_string.end, dom_string.buffer
     );
     
-    if (input.isEmpty() || input == null) input = "";
+    try {
 
-    String hostname = null;
-    String assigned = null;
-    String subdomain = null;
-    String tld = null;
+      org.apache.drill.exec.expr.holders.VarCharHolder row =  new org.apache.drill.exec.expr.holders.VarCharHolder();
 
-    input = input.toLowerCase();
-    input = input.trim();
+      String hostname = null;
+      String assigned = null;
+      String subdomain = null;
+      String tld = null;
 
-    if (input.length() > 0) {
+      input = input.toLowerCase();
+      input = input.trim();
 
-      if (input.indexOf(" ") == -1) { // could add more invalid name checks
+      if (input.length() > 0) {
 
-        crawlercommons.domains.EffectiveTldFinder.EffectiveTLD tld_i = crawlercommons.domains.EffectiveTldFinder.getEffectiveTLD(input);
+        if (input.indexOf(" ") == -1) { // could add more invalid name checks
 
-        if (tld_i != null) {
-  
-          tld = tld_i.getDomain();
-          assigned = crawlercommons.domains.EffectiveTldFinder.getAssignedDomain(input);
-  
-          String remainder = input.substring(0, input.lastIndexOf(assigned));
-  
-          // remove trailing "." if it exists
-          if (remainder.endsWith(".")) remainder = remainder.substring(0, remainder.length()-1);
-  
-          if (remainder.indexOf(".") == -1) { // no subdomains
-            if (remainder.length() > 0) hostname = remainder;
-          } else {
-            String parts[] = remainder.split("\\.", 2);
-            subdomain = parts[1];
-            hostname = parts[0];
+          crawlercommons.domains.EffectiveTldFinder.EffectiveTLD tld_i = crawlercommons.domains.EffectiveTldFinder.getEffectiveTLD(input);
+
+          if (tld_i != null) {
+    
+            tld = tld_i.getDomain();
+            
+            assigned = crawlercommons.domains.EffectiveTldFinder.getAssignedDomain(input);
+    
+            String remainder = input.substring(0, input.lastIndexOf(assigned));
+    
+            // remove trailing "." if it exists
+            if (remainder.endsWith(".")) remainder = remainder.substring(0, remainder.length()-1);
+    
+            if (remainder.indexOf(".") == -1) { // no subdomains
+              if (remainder.length() > 0) hostname = remainder;
+            } else {
+              String parts[] = remainder.split("\\.", 2);
+              subdomain = parts[1];
+              hostname = parts[0];
+            }
+            
           }
-          
+    
         }
-  
+
       }
 
+      if (hostname != null) {
+        byte[] hostBytes = hostname.getBytes();
+        buffer.reallocIfNeeded(hostBytes.length); 
+        buffer.setBytes(0, hostBytes);
+        row.start = 0; 
+        row.end = hostBytes.length; 
+        row.buffer = buffer;
+        mw.varChar("hostname").write(row);
+      }
+
+      if (assigned != null) {
+        byte[] assignBytes = assigned.getBytes();
+        buffer.reallocIfNeeded(assignBytes.length); 
+        buffer.setBytes(0, assignBytes);
+        row.start = 0; 
+        row.end = assignBytes.length; 
+        row.buffer = buffer;
+        mw.varChar("assigned").write(row);
+      }
+
+      if (subdomain != null) {
+        byte[] subBytes = subdomain.getBytes();
+        buffer.reallocIfNeeded(subBytes.length); 
+        buffer.setBytes(0, subBytes);
+        row.start = 0; 
+        row.end = subBytes.length; 
+        row.buffer = buffer;
+        mw.varChar("subdomain").write(row);
+      }
+
+      if (tld != null) {
+        byte[] tldBytes = tld.getBytes();
+        buffer.reallocIfNeeded(tldBytes.length); 
+        buffer.setBytes(0, tldBytes);
+        row.start = 0; 
+        row.end = tldBytes.length; 
+        row.buffer = buffer;
+        mw.varChar("tld").write(row);
+      }
+
+    } catch (Exception e) { 
     }
 
-    org.apache.drill.exec.expr.holders.VarCharHolder row;
-    byte[] outBytes;
-
-    if (hostname != null) {
-      row = new org.apache.drill.exec.expr.holders.VarCharHolder();
-      outBytes = hostname.getBytes();
-      buffer.reallocIfNeeded(outBytes.length); 
-      buffer.setBytes(0, outBytes);
-      row.start = 0; 
-      row.end = outBytes.length; 
-      row.buffer = buffer;
-      mw.varChar("hostname").write(row);
-    }
-
-    if (assigned != null) {
-      row = new org.apache.drill.exec.expr.holders.VarCharHolder();
-      outBytes = assigned.getBytes();
-      buffer.reallocIfNeeded(outBytes.length); 
-      buffer.setBytes(0, outBytes);
-      row.start = 0; 
-      row.end = outBytes.length; 
-      row.buffer = buffer;
-      mw.varChar("assigned").write(row);
-    }
-
-    if (subdomain != null) {
-      row = new org.apache.drill.exec.expr.holders.VarCharHolder();
-      outBytes = subdomain.getBytes();
-      buffer.reallocIfNeeded(outBytes.length); 
-      buffer.setBytes(0, outBytes);
-      row.start = 0; 
-      row.end = outBytes.length; 
-      row.buffer = buffer;
-      mw.varChar("subdomain").write(row);
-    }
-
-    if (tld != null) {
-      row = new org.apache.drill.exec.expr.holders.VarCharHolder();
-      outBytes = tld.getBytes();
-      buffer.reallocIfNeeded(outBytes.length); 
-      buffer.setBytes(0, outBytes);
-      row.start = 0; 
-      row.end = outBytes.length; 
-      row.buffer = buffer;
-      mw.varChar("tld").write(row);
-    }
-    
   }
   
 }
